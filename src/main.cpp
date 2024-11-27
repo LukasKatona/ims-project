4531c4cecceba2a1c1bb6509bf4d1ad43445dd1f
Lukas Katona <katona.lukasek@gmail.com>
2024-11-27 10:31:16 +0100

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,127 @@
-////////////////////////////////////////////////////////////////////////////
-// Model ball                   SIMLIB/C++
-//
-// Bouncing ball (combined simulation  model, variant 1)
-// Simple: zero ball diameter, bounce approximated by state-event
-//
-
 #include "simlib.h"
+#include <string.h>
 
-const double g = 9.81;          // gravity acceleration
+// Model inputs
+double postArrivalTime = 10;
+double addArrivalTime = 100;
+double attentionSpan = 40;
+double lengthOfPost = 20;
 
-class Ball : ConditionDown {    // ball model description
-  Integrator v,y;               // state variables
-  unsigned count;               // bounce event count
-  void Action()  {              // state event description
-      Print("# Bounce#%u:\n", ++count);
-      Out();                    // print state
-      v = -0.8 * v.Value();     // the energy loss
-      y = 0;            // this is needed for detection when small energy!
-      if(count>=20)             // after 20 bounces:
-        Stop();                 //   end simulation run
-  }
-public:
-  Ball(double initialposition) :
-    ConditionDown(y),           // bounce condition: (y>=0) from TRUE to FALSE
-    v(-g),                      // y' = INTG( - m * g )
-    y(v, initialposition),      // y  = INTG( y' )
-    count(0) {}                 // init bounce count
-  void Out() {
-    Print("%-9.3f  % -9.3g  % -9.3g\n",
-          T.Value(), y.Value(), v.Value());
+
+// Global objects
+Facility  User("User");
+Histogram PostTable("Table of time posts spent in system",0,25,20);
+Histogram AddTable("Table of time adds spent in system",0,25,20);
+
+// Variables
+int postCount = 0;
+
+int addCount = 0;
+int addCount6 = 0;
+int addFatigue6 = 0;
+
+int addCount11 = 0;
+int addFatigue11 = 0;
+
+class Post : public Process {
+  double ArrivalTime;
+  void Behavior() {
+    ArrivalTime = Time;
+    Seize(User);    
+    
+    postCount++;
+
+    // TODO add logic 
+    Wait(40);   
+    
+    Release(User);
+    PostTable(Time-ArrivalTime);
   }
 };
 
-Ball m1(1.0);                   // model of system
+class Add : public Process {
+  double ArrivalTime;
+  void Behavior() {
+    ArrivalTime = Time;
+    Seize(User);    
+    
+    addCount++;
+    addCount6++;
+    addCount11++;
 
-void Sample() { m1.Out(); }     // output the ball state periodically
-Sampler S(Sample,0.01);
+    if (addCount6 > 5) {
+      addCount6 = 0;
+      addFatigue6++;
+    }
 
-int main() {                    // experiment description
-  SetOutput("main.dat");
-  Print("# Ball --- model of bouncing ball\n");
-  Print("# Time y v \n");
-  Init(0);                      // initialize experiment
-  SetStep(1e-10,0.5);           // bisection needs small minstep
-  SetAccuracy(1e-5,0.001);      // set numerical error tolerance
-  Run();                        // run simulation, print results
-  SIMLIB_statistics.Output();   // print run statistics
+    if (addCount11 > 10) {
+      addCount11 = 0;
+      addFatigue11++;
+    }
+
+    Print("Add seeing\n");
+
+    // TODO add logic 
+    Wait(20);   
+    
+    Release(User);
+    AddTable(Time-ArrivalTime);
+  }
+};
+
+class PostGenerator : public Event {
+  void Behavior() {
+    (new Post)->Activate();
+    Activate(Time+Exponential(postArrivalTime));
+  }
+};
+
+class AddGenerator : public Event {
+  void Behavior() {
+    (new Add)->Activate();
+    Activate(Time+Exponential(addArrivalTime));
+  }
+};
+
+// periiodicaly decrease fatigue
+class AddFatigue6Digester : public Event {
+  void Behavior() {
+    if (addCount6 > 0) {
+      addCount6--;
+      Print("Decrease fatigue 6\n");
+      AddFatigue6Digester::Activate(Time+(24*60*60/6));
+    }
+  }
+};
+
+// periiodicaly decrease fatigue
+class AddFatigue11Digester : public Event {
+  void Behavior() {
+    if (addCount11 > 0) {
+      addCount11--;
+      Print("Decrease fatigue 11\n");
+      AddFatigue11Digester::Activate(Time+(24*60*60/11));
+    }
+  }
+};
+
+int main() {
+  SetOutput("main.out");
+  Print(" Model simulating user activity on social media, keeping track of how many adds user sees in comparison to posts.\n");
+  Init(0,24*60*60);
+  (new PostGenerator)->Activate();
+  (new AddGenerator)->Activate();
+  (new AddFatigue6Digester)->Activate(24*60*60/6);
+  (new AddFatigue11Digester)->Activate(24*60*60/11);
+  Run();
+  // User.Output();
+  // PostTable.Output();
+  // AddTable.Output();
+  // SIMLIB_statistics.Output();
+
+  Print("Post saw: %d\n", postCount);
+  Print("Add user saw: %d\n", addCount);
+  Print("Fatigue 6: %d\n", addFatigue6);
+  Print("Fatigue 11: %d\n", addFatigue11);
 }
+
